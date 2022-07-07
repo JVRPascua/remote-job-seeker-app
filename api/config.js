05177b49e98c7a74b0125754ef08461093add293
Vincent <pascuavincentjohn@gmail.com>
2022-07-07 08:25:41 +0800

    first commit

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ID: '21b3c1f5',
+  API_KEY: 'afeafbea0c553a15a42812a629bb527e',
+  BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+};
